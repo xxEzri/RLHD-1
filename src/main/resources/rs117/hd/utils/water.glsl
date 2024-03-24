c0710ab14619ade707cd56279a7ae1cad1d1ecd8
Ezri <73786759+xxEzri@users.noreply.github.com>
2024-03-24 11:37:37 +0000

    Update water.glsl

--- src/main/resources/rs117/hd/utils/water.glsl
+++ src/main/resources/rs117/hd/utils/water.glsl
@@ -480,8 +480,8 @@ void sampleUnderwater(inout vec3 outputColor, WaterType waterType, float depth, 
 //    outputColor = vec3(0); return;
 
     if (underwaterCaustics) {
-        const float scale = 2;
-        const float maxCausticsDepth = 128 * 7;
+        const float scale = 2.25;
+        const float maxCausticsDepth = 128 * 6;
 
         vec2 causticsUv = worldUvs(scale);
 
@@ -496,7 +496,7 @@ void sampleUnderwater(inout vec3 outputColor, WaterType waterType, float depth, 
         vec2 flow2 = causticsUv * 1.5 + animationFrame(23) * -direction;
         vec3 caustics = sampleCaustics(flow1, flow2, .005);
 
-        vec3 causticsColor = underwaterCausticsColor * underwaterCausticsStrength *0.7;
+        vec3 causticsColor = underwaterCausticsColor * underwaterCausticsStrength *0.75;
         outputColor.rgb *= 1 + caustics * causticsColor * depthMultiplier * lightDotNormals * lightStrength;
     }
 
