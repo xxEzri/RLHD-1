79bc4e1c3a59dfc8bb132ba85268c9bd3499efb8
Hooder <ahooder@protonmail.com>
2024-04-06 16:21:25 +0200

    Add texture tiling license info

--- src/main/resources/rs117/hd/utils/texture_tiling.glsl
+++ src/main/resources/rs117/hd/utils/texture_tiling.glsl
@@ -1,3 +1,6 @@
+// The MIT License
+// Copyright © 2017 Inigo Quilez
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #pragma once
 
 // https://iquilezles.org/articles/texturerepetition/
@@ -68,7 +71,7 @@ vec4 textureNoTile( sampler2DArray samp, in vec3 uvw )
     return va/wt;
 }
 
-// from http://www.java-gaming.org/index.php?topic=35123.0
+// Written by theagentd, from https://web.archive.org/web/20200417234120/http://www.java-gaming.org/index.php?topic=35123.0
 vec4 cubic(float v){
     vec4 n = vec4(1.0, 2.0, 3.0, 4.0) - v;
     vec4 s = n * n * n;
